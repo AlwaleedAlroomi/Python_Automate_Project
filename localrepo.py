d51bcf9c3406c9f03b8d048dff69ae94eadc9f99
AlwaleedAlroomi <alwaleed101az@gmail.com>
2021-01-28 08:43:41 +0300

    Edit some issues

--- localrepo.py
+++ localrepo.py
@@ -5,24 +5,21 @@ from environs import Env
 
 env = Env()
 env.read_env()  # read the .env file
-repoName = sys.argv[1]  # get the repo name
+commitMess = sys.argv[1]  # get the commit message in CamelCase
 token = env("gt")  # get the token from the .env file
-rp = os.environ["ap"] = sys.argv[2]  # set the project path to the .env file
+os.environ["ap"] = sys.argv[2]  # set the project path to the .env file
 path = env("ap")  # get the project path from the .env file
 
 g = Github(token)
 user = g.get_user()
 login = user.login
-repo = user.create_repo(repoName)
 
 commands = [
-            f'echo"# {repo.name}" >> README.md',
-            'git init',
             'git add *',
-            'git commit -m "Initial Commit"',
+            'git status',
             'git branch -M main',
-            f'git remote add origin https://github.com/{login}/{repoName}.git',
-            'git push -u origin main',
+            f'git commit -m "{commitMess}"',
+            'git push origin main',
         ]
 
 if sys.argv[2] is not None:
